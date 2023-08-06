7b8f77cc70500e0a69497d6ce968b1d2d44ab994
emannuel <emannuelloxt@hotmail.com>
2023-08-06 12:55:10 -0400

    test: get customer by id

---
+++ domain/customer/memory/memory_test.go
@@ -0,0 +1,54 @@
+package memory
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"github.com/loxt/tavern-ddd/aggregate"
+	"github.com/loxt/tavern-ddd/domain/customer"
+	"testing"
+)
+
+func TestMemory_GetCustom(t *testing.T) {
+	type testCase struct {
+		name        string
+		id          uuid.UUID
+		expectedErr error
+	}
+
+	cust, err := aggregate.NewCustomer("Emannuel")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id := cust.GetID()
+
+	repo := MemoryRepository{
+		customers: map[uuid.UUID]aggregate.Customer{
+			id: cust,
+		},
+	}
+
+	testCases := []testCase{
+		{
+			name:        "no customer by id",
+			id:          uuid.MustParse("76453458-232b-4ed3-90c5-3c2cabcef52a"),
+			expectedErr: customer.ErrCustomerNotFound,
+		},
+		{
+			name:        "customer by id",
+			id:          id,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := repo.Get(tc.id)
+
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected error %v, got %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
